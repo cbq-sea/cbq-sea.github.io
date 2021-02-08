80c658859978417cb0460c070ab47b4f55f52035
cuibq <cuibq@shinemo.com>
2021-02-08 17:35:12 +0800

    build

---
+++ .umirc.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: '崔的blog',
+  mode: 'site',
+  locales: [['zh-CN', '中文']],
+  outputPath: 'blog',
+  // logo: '/xrk.jpeg',
+  // more config: https://d.umijs.org/config
+});
---
+++ docs/getting-started.md
@@ -0,0 +1,9 @@
+---
+nav:
+  title: 开始阅读
+  order: 1
+---
+
+# node
+
+Balabala...
